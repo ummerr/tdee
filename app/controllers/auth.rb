61d50a10dd87afb72f3191cd3ae4546c460fbdc1
ummerr <ummerr@gmail.com>
2015-01-24 15:14:33 -0500

    updated controller logic for user authentication

--- app/controllers/auth.rb
+++ app/controllers/auth.rb
@@ -3,9 +3,38 @@ get '/' do
 end
 
 get '/login' do
-  
+  erb :'auth/login'
+end
+
+post '/login' do
+  user = User.find_by(name: params[:user][:name])
+
+  if user.try(:authenticate, params[:user][:password])
+    session[:user_id] = user.id
+    redirect '#'
+  else
+    redirect '/login'
+  end
+
 end
 
 get '/signup' do
-  
+  erb :'auth/signup'
+end
+
+post '/signup' do
+  user = User.create(params[:user])
+
+  if user.save
+    session[:user_id] = user.id
+    redirect '#'
+  else
+    redirect '/signup'
+  end
+
+end
+
+get '/logout' do
+  session[:user_id] = nil
+  redirect '/'
 end
