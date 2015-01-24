e7b004c9e4be622ccdd75f5e62181774ae43f2b4
ummerr <ummerr@gmail.com>
2015-01-24 15:05:01 -0500

    added a seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,9 @@
-faker = User.new()
+10.times do
+  faker = User.new(name: Faker::Name.name, password: '123', password: '123', email: Faker::Internet.email, birthday: '1986/01/01')
+  2.times do
+    report = Report.create(intake: Faker::Number.number(4))
+    faker.reports << report
+  end
+  faker.save
+end
+
