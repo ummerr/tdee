8bed8262160341259b4c663eb5b3937eb996b0e8
ummerr <ummerr@gmail.com>
2015-01-24 17:10:49 -0500

    added a show method for reports

--- app/controllers/report.rb
+++ app/controllers/report.rb
@@ -5,7 +5,7 @@ end
 post '/reports' do
   @report = Report.create(params[:report])
   current_user.reports << @report
-  redirect '/reports'
+  redirect "/reports/#{@report.id}"
 end
 
 get '/reports/all' do
@@ -13,7 +13,8 @@ get '/reports/all' do
 end
 
 get '/reports/:id' do |id|
-
+  @report = Report.find(id)
+  erb :'reports/show'
 end
 
 delete '/reports/:id' do |id|
