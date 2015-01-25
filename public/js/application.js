3147058d76ab653b43791ef315d14624209389ad
Amar Singh <ummerr@gmail.com>
2015-01-25 18:46:03 -0500

    added a jquery call to display hidden form

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  $('button#show-report-form').click(function() {
+    $('#create-report-page').show('slow');
+    $(this).hide();
+  });
+
 });
