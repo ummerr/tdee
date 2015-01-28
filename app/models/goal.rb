0d4b4019b6b3f81dc6c5c98da1c6c32d8e3a20ed
Amar Singh <ummerr@gmail.com>
2015-01-27 19:33:29 -0500

    added associations between goals and users

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,3 +1,3 @@
 class Goal < ActiveRecord::Base
-  # Remember to create a migration!
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
   has_secure_password
   has_many :reports
+  has_many :goals
 end
